d4d877382f9877d305384b51f4d8c67597eefadf
Stephan <elondostephan@gmail.com>
2018-11-06 13:38:57 -0700

    first

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <footer className="bg-dark text-white mt-5 p-4 text-center">
+      <div>
+      <p>DevFinder © 2018</p>
+      </div>
+    
+    </footer>
+  );
+};
